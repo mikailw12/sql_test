d6cee1a14ffd5f1f6de0f927cd275882a9ceb2b5
woodyden <woodyden2009@mail.ru>
2024-10-31 17:22:58 +0300

    companies info

--- main.py
+++ main.py
@@ -1,24 +1,59 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import relationship
- 
-sqlite_database = 'sqlite:///metanit2.db'
+from sqlalchemy import create_engine, Integer, String, ForeignKey, Column
+from sqlalchemy.orm import Session, DeclarativeBase, relationship
+
+sqlite_database = 'sqlite:///metanit.db'
+
 engine = create_engine(sqlite_database)
- 
-class Base(DeclarativeBase): pass
+
+class Base(DeclarativeBase):
+    pass
+
 class User(Base):
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-    company_id = Column(Integer, ForeignKey('companies.id'))
-    company = relationship('Company', back_populates='user')
+    company_id = Column(String, ForeignKey('companies.id'))
 
- 
+    company = relationship('Company', back_populates='users')
+
 class Company(Base):
     __tablename__ = 'companies'
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-    users = relationship('User', back_populates='company')
- 
 
-Base.metadata.create_all(bind=engine)
+    users = relationship('User', back_populates='company')
+    products = relationship('Product', back_populates='company')
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    company_id = Column(String, ForeignKey('companies.id'))
+
+    company = relationship('Company', back_populates='products')
+
+
+
+
+Base.metadata.create_all(engine)
+
+
+with Session(autoflush=False, bind=engine) as db:
+    pass
+
+
+    companies = db.query(Company).all()
+    for company in companies:
+        print(f'Компания- {company.name}')
+        print('Сотрудники: ')
+        for user in company.users:
+            print(f'{user.name}')
+        print('Продукты: ')
+        for product in company.products:
+            print(f'{product.name}')
+
+        print("\n" + "-"*30 + "\n")    
